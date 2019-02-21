d70a3d1772325d9d754fac10f3e6cb7b87d98471
Chao Hsuan Chi <allen0413@gmail.com>
2019-02-21 05:09:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,14 +5,14 @@ class Student
     new_student = self.new
     new_student.id = row[0]
     new_student.name =  row[1]
-    new_student.length = row[2]
+    new_student.grade = row[2]
     new_student
   end
 
   def self.all
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
     SQL
 
     DB[:conn].execute(sql).map do |row|
@@ -21,8 +21,16 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
